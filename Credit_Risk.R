e05962475f5dc59a2523ca82760df068d4101ba3
Norina Sun <norina_sun@yahoo.ca>
2020-10-15 23:33:36 -0400

    version 1.2.11: best performing model with 0.45 split

--- Credit_Risk.R
+++ Credit_Risk.R
@@ -31,6 +31,9 @@ process <- function(df,df_type) {
   #dropping type since its all auto
   df <- subset(df, select = -c(TYP_FIN))
   
+  #just a few variables
+  df$costincome = df$MNT_DEMANDE / df$REV_NET
+  
   if (df_type == "train"){
     #dropping the id column
     df <- subset(df, select= -c(ID_TRAIN))
@@ -62,54 +65,27 @@ train_Y <- train_processed.data$target_0
 test_Y <- train_processed.data$target_0
 
 #specifying model parameters
-objControl <- trainControl(method='cv', number=3, returnResamp='none', summaryFunction = twoClassSummary, classProbs = TRUE, sampling="up")
+objControl <- trainControl(method='repeatedcv', number=3, returnResamp='none', summaryFunction = twoClassSummary, classProbs = TRUE, sampling="up")
 
 #fitting the model
 model <- train(train_X, train_Y, method ="gbm", trControl=objControl, metric="ROC")
 
-#getting the prediction probabilities
-test_predictions <- predict(object=model, test_X, type='prob')
-
-#adding the predictions to the test dataframe
-test_X$nodefault_prob <- test_predictions$no_default
-
-#checking varying thresholds for the prediction probabilities 
-threshold_list = seq(0,1,0.01)
-list_results <- list()
-test_X$true <- test_Y
-
-for (i in threshold_list) {
-  threshold <- i
-  sum_results <- 0
-  #creating prediction column
-  test_X$decision <- ifelse(test_X$nodefault_prob >= threshold, "no_default", "default")
-  #creating column for if its a positive match 
-  test_X$pos_match <- ifelse(test_X$decision == "no_default" & test_X$true == "no_default",1,0)
-  #creating column for false positive
-  test_X$false_pos <- ifelse(test_X$decision == "no_default" & test_X$true == "default",1,0)
-  #creating data table of correct predictions
-  test_X_correct <- test_X[test_X$pos_match == 1,]
-  #creating data table of false positives
-  test_X_falsepos <- test_X[test_X$false_pos == 1,]
-  #getting the sum
-  profit = sum(test_X_correct$MNT_DEMANDE) - sum(test_X_falsepos$MNT_DEMANDE)
-  #printing the results
-  cat(threshold,":", profit, "|")
-}
-  
 #using the test set
 test_predictions <- predict(object=model, test_X, type='raw')
 postResample(pred=test_predictions, obs=as.factor(test_Y))
 
 #prediction with the final dataset
 to_predict_processed <- process(to_predict,"test")
-predictions<- predict(object=model, to_predict_processed, type='prob')
+predictions_final <- predict(object=model, to_predict_processed, type='raw')
+predictions_prob <- predict(object=model, to_predict_processed, type='prob')
 
-#using threshold of 0.2
-to_predict$prediction <- ifelse(test_X$nodefault_prob >= 0.45, "no_default", "default")
+#adding the predictions to the dataframe
+to_predict$prediction <- predictions_final
+to_predict$prediction_prob <- predictions_prob$no_default
+to_predict$prediction_threshold <- ifelse(to_predict$prediction_prob >= 0.455, "no_default","default")
 
 #taking just the yesses
-good_loans = to_predict[to_predict$prediction == "no_default",]
+good_loans = to_predict[to_predict$prediction_threshold == "no_default",]
 
 #saving the table
 write.csv(good_loans$ID_TEST,"/Users/NorinaSun/Downloads/MATH60603/CREDIT_RISK/CreditGameData/good_loans.csv")
